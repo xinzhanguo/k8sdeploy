2b7ef933587c2da5b8f3ac3ca9fbb7a497f11dc4
euclid <e@xinzhanguo.cn>
2024-06-28 12:56:53 +0800

    fixed

--- resource/gen.py
+++ resource/gen.py
@@ -1,16 +1,23 @@
 from jinja2.nativetypes import NativeEnvironment
 import group
 import json
+import sys
 
 
-fname = 'etcd-csr.json'
-env = NativeEnvironment()
-f = open(fname+'.j2', 'r')
-c = f.read()
-t = env.from_string(c)
-result = t.render(groups=group.groups)
-c = result
-if type(result)==dict:
-    c = json.dumps(result)
-with open(fname, "w") as fh:
-    fh.write(c)
+def gen(fname: str):
+    env = NativeEnvironment()
+    f = open(fname+'.j2', 'r')
+    c = f.read()
+    t = env.from_string(c)
+    result = t.render(groups=group.groups)
+    c = result
+    if type(result)==dict:
+        c = json.dumps(result)
+    with open(fname, "w") as fh:
+        fh.write(c)
+
+def main():
+    if len(sys.argv)>=2:
+        gen(sys.argv[1])
+
+main()
